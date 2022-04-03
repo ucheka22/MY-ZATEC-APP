c5ce911584c6ac3850ccafac1bfdd25c1d49b450
Ucheka Chike <ryanucheka@gmail.com>
2022-04-03 15:04:25 +0100

    initial commit

---
+++ src/main/java/com/chike/zatechapp/ZatechAppApplication.java
@@ -0,0 +1,13 @@
+package com.chike.zatechapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZatechAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZatechAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chike/zatechapp/ZatechAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chike.zatechapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZatechAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
