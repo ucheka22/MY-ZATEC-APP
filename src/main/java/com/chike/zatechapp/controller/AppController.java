0eba2fa845ef158408f63b5cab7f897f9382e693
Ucheka Chike <ryanucheka@gmail.com>
2022-04-09 21:56:28 +0100

    swagger docs config

--- src/main/java/com/chike/zatechapp/controller/AppController.java
+++ src/main/java/com/chike/zatechapp/controller/AppController.java
@@ -1,27 +1,36 @@
 package com.chike.zatechapp.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("api/v1")
+@Tag(name = "APP Controller", description = "Get Jokes Category & Star War People")
 public class AppController {
 
+    @Operation(summary = "Get All Chuck Norris Jokes Categories")
+    @ResponseStatus(code = HttpStatus.OK)
     @GetMapping("/chuck/categories")
-    public String getJokesCategories(){
-        return  "Hello World";
+    public String getJokesCategories() {
+        return "Hello World";
     }
 
     @GetMapping("/swapi/people")
-    public String getSwapiPeople(){
-        return  "Hello World";
+    public String getSwapiPeople() {
+        return "Hello World";
     }
 
     @GetMapping("/search")
-    public String search(@RequestParam("query") String query){
-        return  "Hello World";
+    public String search(@Parameter(description = "The search query string") @RequestParam("query") String query) {
+        return "Hello World";
     }
 
 }
