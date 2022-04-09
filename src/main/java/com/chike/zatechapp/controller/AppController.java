05ad308d2f43e57e7378c4254085b7e701d9b431
Ucheka Chike <ryanucheka@gmail.com>
2022-04-09 09:53:21 +0100

    change route

--- src/main/java/com/chike/zatechapp/controller/AppController.java
+++ src/main/java/com/chike/zatechapp/controller/AppController.java
@@ -6,10 +6,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1")
 public class AppController {
 
-    @GetMapping("/chuck/categories")
+    @GetMapping("/")
     public String ping(){
         return  "Hello World";
     }
